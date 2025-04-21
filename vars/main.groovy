c341622ad3df7ccdec219dd03b14a6e6415f21fa
DevSecOps <55474290+trainmefordevsecops@users.noreply.github.com>
2025-04-21 23:38:43 +0800

    Update main.groovy

--- vars/main.groovy
+++ vars/main.groovy
@@ -1,7 +1,8 @@
-def call(jobname) {
+def call(String jobname) {
 
     echo "Running job: ${jobname}"
     dir("${env.WORKSPACE}") {
+        sh 'chmod +x ./gradlew'
         sh './gradlew clean build'
     }
 
