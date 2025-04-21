865cc9abdc7dd612a2621396c8b3d45eed317412
DevSecOps <55474290+trainmefordevsecops@users.noreply.github.com>
2025-04-22 02:20:35 +0800

    fix npm run build cmd

--- vars/NodejsBuild.groovy
+++ vars/NodejsBuild.groovy
@@ -3,7 +3,7 @@ def call(String jobname) {
     echo "Running job: ${jobname}"
     dir("${env.WORKSPACE}") {
         sh 'npm install'
-        sh 'npm build'
+        sh 'npm run build'
     }
 
 } 
